a45b26c627e82b4bcbf0d016113d477cbc3d08b3
Aleksi Malkki <aleksi.malkki@gmail.com>
2024-10-26 02:45:34 +0300

    Server setup

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
-  outputDir: '\\\\pinas.local\\WebserverDev',
-  transpileDependencies: true
-})
+  // Check the environment
+  outputDir: process.env.NODE_ENV === 'production'
+    ? '\\\\pinas.local\\WebserverDev' // Network drive for production build
+    : path.resolve(__dirname, 'dist'), // Local folder for development (you can change 'dist' to any folder you prefer)
+
+  transpileDependencies: true,
+
+  devServer: {
+    // You can configure devServer options here if needed
+    // contentBase: path.join(__dirname, 'dist'), // Serve files from 'dist' directory during development
+    // compress: true,
+    port: 80
+  }
+});
