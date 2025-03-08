28eeb21600b5f99613b11dad2b7c11f741a4f195
Aleksi Malkki <aleksi.malkki@gmail.com>
2025-03-08 15:16:29 +0200

    Preview site disclaimer notification

--- src/utils/dataQuery.js
+++ src/utils/dataQuery.js
@@ -6,6 +6,7 @@ import { STATIC_CONTENT } from '@/data/staticData.js';
 
 const standAloneBuild = process.env.VUE_APP_STANDALONE_BUILD == "true";
 if (standAloneBuild) {
+    notify("Please note: This app is in standalone mode. Most features are disabled, but you can browse the site to explore its look and feel.", "info", 15000)
     console.warn("Note: This app is in standalone mode. Most features are disabled, but you can browse the site to explore its look and feel.")
 }
 
