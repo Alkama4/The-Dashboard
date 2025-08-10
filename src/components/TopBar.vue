ff5bd1f68af481ab957063d85090f167dd63b4d3
Aleksi Malkki <aleksi.malkki@gmail.com>
2025-08-10 14:40:49 +0300

    Rework desktop and mobile navigation

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,40 +1,41 @@
 <template>
-    <div class="top-bar-wrapper">
-        <div class="top-bar desktop-nav">
-            <div class="side">
-                <router-link 
-                    to="/" 
-                    class="no-decoration website-name"
-                >
-                    The Dashboard
-                </router-link>
-            </div>
-            <div class="side">
-                <div
-                    v-for="(link, index) in links" 
-                    :key="index"
-                    class="primary-div desktop-only"
-                >
+    <div class="desktop-nav-wrapper">
+        <div class="top-bar desktop-nav" @mouseleave="handleMouseleave()">
+            <div class="primary-row">
+                <div class="side">
+                    <router-link 
+                        to="/" 
+                        class="no-decoration website-name"
+                    >
+                        The Dashboard
+                    </router-link>
+                </div>
+                <div class="side">
                     <router-link
+                        v-for="(link, index) in links" 
+                        :key="index"
                         :to="link.to"
-                        class="no-decoration text-button primary-link"
+                        class="no-decoration text-button primary-link desktop-only"
                         :class="{ 'category-active': $route.path === link.to || $route.path.startsWith(link.to) }"
+                        @mouseover="handleMouseover(index)"
                     >
                         {{ link.display }}
                     </router-link>
-                    <div class="children" v-if="link?.children.length > 0">
-                        <router-link 
-                            v-for="(child, index) in link.children" 
-                            :key="index" 
-                            :to="child.to"
-                            class="no-decoration text-button child"
-                        >
-                            {{ child.display }}
-                        </router-link>
-                    </div>
+                    <IconDarkMode class="icon-button desktop-only" @click="localToggleDarkMode"/>
+                    <IconMenu size="28px" class="icon-button mobile-only" @click="toggleMenu"/>
                 </div>
-                <IconDarkMode class="icon-button desktop-only" @click="localToggleDarkMode"/>
-                <IconMenu size="28px" class="icon-button mobile-only" @click="toggleMenu"/>
+            </div>
+            <div class="secondary-row" ref="secondaryRow">
+                <div class="secondary-row-content" ref="secondaryRowContent">
+                    <router-link 
+                        v-for="(link, index) in links[mouseoverIndex]?.children" 
+                        :key="index"
+                        class="no-decoration text-button"
+                        :to="link.to"
+                    >
+                        {{ link.display }}
+                    </router-link>
+                </div>
             </div>
         </div>
         <div 
@@ -52,67 +53,27 @@
             <div
                 v-for="(link, index) in links" 
                 :key="index"
-                class="primary-div"
+                class="primary-area"
             >
                 <router-link
                     :to="link.to"
                     class="no-decoration text-button primary-link"
-                    >
-                    {{ link.display }}
-                </router-link>
-                <div class="children" v-if="link.children.length > 0">
-                    <router-link 
-                        v-for="(child, index) in link.children" 
-                        :key="index" 
-                        :to="child.to"
-                        class="no-decoration text-button child"
-                    >
-                        {{ child.display }}
-                    </router-link>
-                </div>
-            </div>
-            <IconDarkMode class="icon-button" @click="localToggleDarkMode"/>
-        </div>
-    </div>
-</template>
-
-    <!-- <div id="top-bar">
-        <div id="top-bar-content">
-            <div class="top-bar-side">
-                <router-link class="website-name" to="/">The Dashboard</router-link>
-            </div>
-            <div class="top-bar-side">
-                <router-link 
-                    v-for="link in links" 
-                    :key="link.to" 
-                    class="nav-button desktop-button" 
-                    :to="link.to"
                 >
                     {{ link.display }}
                 </router-link>
-                <button @click="localToggleDarkMode()" class="nav-button desktop-button square-button">
-                    <IconDarkMode color="var(--color-button-nav)" color-hover="var(--color-button-nav-hover)"/>
-                </button>
-                <button @click="toggleMenu()" class="nav-button mobile-button square-button">
-                    <IconMenu color="var(--color-button-nav)" color-hover="var(--color-button-nav-hover)"/>
-                </button>
+                <router-link 
+                    v-for="(child, index) in link.children" 
+                    :key="index" 
+                    :to="child.to"
+                    class="no-decoration text-button primary-link"
+                >
+                    {{ child.display }}
+                </router-link>
             </div>
+            <IconDarkMode class="icon-button dark-mode-toggle" @click="localToggleDarkMode"/>
         </div>
     </div>
-    <div @click="toggleMenu()" ref="mobileNav" class="mobile-nav backdrop">
-        <router-link class="website-name" to="/" style="padding-bottom: var(--spacing-md);">The Dashboard</router-link>
-        <router-link 
-            v-for="link in links" 
-            :key="link.to" 
-            class="nav-button" 
-            :to="link.to"
-        >
-            {{ link.display }}
-        </router-link>
-        <button @click="localToggleDarkMode(); toggleMenu()" class="nav-button square-button">
-            <IconDarkMode color="var(--color-button-nav)" color-hover="var(--color-button-nav-hover)"/>
-        </button>
-    </div> -->
+</template>
 
 <script>
 import IconDarkMode from './icons/IconDarkMode.vue';
@@ -150,7 +111,8 @@ export default {
                     to: '/account',
                     children: []
                 }
-            ]
+            ],
+            mouseoverIndex: 0,
         };
     },
     methods: {
@@ -168,13 +130,28 @@ export default {
         },
         localToggleDarkMode() {
             toggleDarkMode();
+        },
+        handleMouseover(index) {
+            this.mouseoverIndex = index;
+            setTimeout(() => {
+                if (this.links[index].children.length === 0) {
+                    this.$refs.secondaryRow.style.height = "0px";
+                } else {
+                    this.$refs.secondaryRow.style.height = this.$refs.secondaryRowContent.scrollHeight + 'px';
+                }
+            }, 1);
+        },
+        handleMouseleave() {
+            this.$refs.secondaryRow.style.height = "0px";
+            this.mouseoverIndex = null;
         }
     }
 };
 </script>
 
 <style scoped>
-.top-bar-wrapper {
+/* This has the actual styling */
+.desktop-nav-wrapper {
     width: 100vw;
     position: fixed;
     top: 0;
@@ -182,12 +159,14 @@ export default {
     display: flex;
     justify-content: center;
     background-color: var(--color-background-top-bar);
+    backdrop-filter: blur(30px);    /* Keep under 1/2 of height */
+    border-bottom: 1px solid var(--color-border);
     z-index: var(--z-top-bar);
 }
 
-.top-bar {
+.desktop-nav {
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: space-between;
     align-items: center;
 
@@ -196,15 +175,35 @@ export default {
 
     width: 100%;
     max-width: var(--width-top-bar);
-    height: 60px;
 
     white-space: nowrap;
 }
-
-.top-bar .side {
+.desktop-nav .primary-row {
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+.desktop-nav .primary-row .side {
     display: flex;
     flex-direction: row;
     align-items: center;
+}
+
+.desktop-nav .secondary-row {
+    width: 100%;
+    transition: height 0.2s var(--cubic-1);
+    height: 0;
+    overflow: hidden;
+}
+.desktop-nav .secondary-row-content {
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    width: 100%;
+    padding-top: var(--spacing-sm);
+    padding-bottom: var(--spacing-md);
+    border-top: 2px solid var(--color-border);
 }
 
 
@@ -215,9 +214,14 @@ export default {
 
 .text-button {
     color: var(--color-text-light);
-    transition: color 0.1s ease-out;
-    font-size: 18px;
+    font-size: var(--font-size-small);
     font-weight: 500;
+    padding: 6px 10px;
+    margin-left: 0;
+    border-radius: var(--border-radius-medium);
+    background-color: #24242400;
+    transition: color 0.1s ease-out,
+                background-color 0.1s ease-out;
 }
 .text-button:hover {
     color: var(--color-text);
@@ -230,68 +234,18 @@ export default {
     padding: var(--spacing-sm);
 }
 
-.primary-div {
-    display: flex;
-    position: relative;
+.desktop-only {
+    display: unset;
 }
-.mobile-nav .primary-div {
-    flex-direction: column;
-    align-items: flex-end;
-}
-.desktop-nav .primary-div {
-    align-items: center;
-    height: var(--height-top-bar);
-    padding-inline: var(--spacing-sm);
-}
-
-.desktop-nav .primary-div .children {
-    background-color: var(--color-background-top-bar);
-    pointer-events: none;
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    top: calc(100% - 0px);
-    left: calc(-1 * var(--spacing-md));
-    min-width: 120px;
-    
-    padding: var(--spacing-md);
-    padding-inline: calc(var(--spacing-md) + var(--spacing-sm));
-    padding-top: 0;
-    border-radius: var(--border-radius-medium);
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-
-    opacity: 0;
-
-    transition: opacity 0.1s ease-out;
-}
-.desktop-nav .primary-div:hover .children {
-    opacity: 1;
-    pointer-events: all;
-}
-
-.desktop-only .primary-div .children .child {
-    width: 100%;
-}
-
-.primary-div .children .child {
-    /* font-weight: 400; */
-    padding: var(--spacing-xs) 0;
-    /* color: var(--color-text-lighter); */
-}
-.primary-div .children .child:hover {
-    color: var(--color-text);
-}
-
 .mobile-only {
     display: none;
 }
-@media (max-width: 750px) {
+@media (max-width: 650px) {
     .mobile-only {
         display: unset;
     }
     .desktop-only {
-        display: none !important;
+        display: none;
     }
 }
 
@@ -301,7 +255,6 @@ export default {
     padding: calc(var(--spacing-lg) + var(--spacing-md)) var(--spacing-lg);
     display: flex;
     flex-direction: column;
-    align-items: flex-end;
 
     opacity: 0;
     pointer-events: none;
@@ -313,40 +266,35 @@ export default {
 }
 
 .mobile-nav .website-name {
-    font-size: 2rem;
+    font-size: var(--font-size-xxl);
+    margin-bottom: var(--spacing-md)
 }
-.mobile-nav .primary-div,
-.mobile-nav .icon-button {
-    margin-top: 32px;
-}
-.mobile-nav .primary-link {
-    /* color: var(--color-text); */
-    font-size: 1.5rem;
-}
-.mobile-nav .children {
+
+.mobile-nav .primary-area {
     display: flex;
     flex-direction: column;
-    align-items: flex-end;
-}
-.mobile-nav .child {
-    margin-top: var(--spacing-xs);
-    /* margin-right: var(--spacing-lg); */
-    text-align: end;
-    color: var(--color-text-light);
-    font-size: 1.2rem;
+    border-top: 2px solid var(--color-border);
+    padding: var(--spacing-sm) 0;
 }
 
-.category-active {
-    color: var(--color-text) !important;
+.mobile-nav .text-button {
+    font-size: var(--font-size-lg);
 }
 
-.router-link-active {
-    /* color: var(--color-text-hidden) !important;
-    background-color: var(--color-text);
-    padding-inline: var(--spacing-md) !important;
-    border-radius: var(--border-radius-small); */
-    
-    color: var(--color-text) !important;
+.mobile-nav .dark-mode-toggle {
+    position: absolute;
+    top: 52px;
+    right: 28px;
+}
+
+/* Active link styling */
+.text-button.router-link-active {
+    color: var(--color-text);
+    background-color: var(--color-button-general);
+}
+.pointer-device .text-button.router-link-active:hover {    
+    color: var(--color-text-bold);
+    background-color: var(--color-button-general-hover);
 }
 
 </style>
