e0d3daac574c2561b6cfc019073aee942f8a0b62
Aleksi Malkki <aleksi.malkki@gmail.com>
2025-09-17 12:31:35 +0300

    TitleCard component clean-up - Removed deprecated styles and organized the remaining ones - Replaced the component’s local getUrl method with the global helper from utils for consistency

--- src/components/WatchList/TitleCard.vue
+++ src/components/WatchList/TitleCard.vue
@@ -7,7 +7,7 @@
                 <MissingImage v-if="imgNotFound"/>
                 <img 
                     v-else
-                    :src="posterUrl(titleDetails.title_id, 600, titleDetails.backup_poster_url)" 
+                    :src="getPosterUrl(titleDetails.title_id, 600, titleDetails.backup_poster_url)" 
                     loading="lazy"
                     @load="(event) => event.target.classList.add('loaded')" 
                     @error="imgNotFound = true"
@@ -57,9 +57,8 @@
 </template>
 
 <script>
-import { convert } from '@/utils/utils';
+import { convert, getMediaImageUrl } from '@/utils/utils';
 import IconTMDB from '@/components/icons/IconTMDB.vue';
-import { standAloneBuild } from '@/utils/config';
 import MissingImage from '@/components/common/MissingImage.vue';
 import DropdownMenu from '../common/DropdownMenu.vue';
 import fastApi from '@/utils/fastApi';
@@ -93,13 +92,8 @@ export default {
         formatRuntime(runtime) {
             return convert.toRuntime(runtime);
         },
-        posterUrl(titleId, width, backupUrl) {
-            if (standAloneBuild) {
-                if (width == 600) width = 500;  // TMDB doesn't have a 600 so use 500 instead
-                return `https://image.tmdb.org/t/p/w${width}${backupUrl}`;
-            } else {
-                return `${process.env.VUE_APP_API_URL}/media/image/title/${titleId}/poster.jpg?width=${width}`;
-            }
+        getPosterUrl(titleId, width, backupUrl) {
+            return getMediaImageUrl(width, backupUrl, titleId);
         },
         async handleToggleFavourite() {
             const response = await fastApi.watch_list.titles.favourite(this.titleDetails.title_id);
@@ -116,14 +110,10 @@ export default {
 .title-card {
     display: flex;
     flex-direction: column;
-    /* background-color: var(--color-background-card); */
     width: 220px;
-    /* height: 400px; */
     position: relative;
     cursor: pointer;
     user-select: none;
-    box-shadow: var(--shadow-card);
-    /* transform: scale(0.95); */
     transition: transform 0.15s var(--cubic-1);
 }
 @media (max-width: 900px) {
@@ -134,14 +124,13 @@ export default {
 
 /* Thumbnail Image */
 .title-card-thumbnail {
-    /* position: absolute; */
     border-radius: var(--border-radius-medium);
     overflow: hidden;
     width: 100%;
     aspect-ratio: 2/3;
     background-color: var(--color-background-card-section);
     z-index: 1;
-
+    box-shadow: var(--shadow-card);
 }
 .title-card-thumbnail img {
     width: 100%;
@@ -156,9 +145,8 @@ export default {
     margin-top: var(--spacing-sm);
     font-weight: 500;
     text-align: left;
-    color: var(--color-text-white-light);
+    color: var(--color-text-light);
     opacity: 1;
-    /* transform: translateX(15px); */
     z-index: 3;
     transition: opacity 0.2s var(--cubic-1), transform 0.2s var(--cubic-1);
 }
@@ -166,7 +154,7 @@ export default {
 .title-card-name {
     font-size: var(--font-size-lg);
     font-weight: 700;
-    color: var(--color-text-white);
+    color: var(--color-text);
     display: -webkit-box;
     line-clamp: 1;
     -webkit-line-clamp: 1;
@@ -190,33 +178,13 @@ export default {
     z-index: 10;
 }
 
-/* Hover and Touch Interactions */
-/* .pointer-device .title-card:hover {
-    transform: scale(1);
-} */
 
-/* .title-card:hover .title-card-details,
-.title-card:hover .title-card-gradient,
-.touch-device .title-card .title-card-details,
-.touch-device .title-card .title-card-gradient {
-    opacity: 1;
-    transform: translateX(0);
-} */
-
+/* Dropdown positioning */
 .dropdown-menu {
     top: var(--spacing-sm);
     right: var(--spacing-xs);
 }
 
-/* Full Width Variant */
-.full-width-title-card {
-    height: calc(300px * 0.95);
-    width: calc(100% - var(--spacing-xl)) !important;
-    margin: calc(300px * 0.025) calc(200px * 0.025);
-    box-sizing: border-box;
-    transform: none !important;
-    cursor: default;
-}
 
 /* Season and Episode Formatting */
 .season-after::after {
